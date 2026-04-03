38f71fd41affc61a7ea53c61cabc58b8d8c839e7
Patryk Wojtkielewicz <p.wojtkielewicz1@gmail.com>
2026-04-03 10:17:14 +0200

    add auth DTOs and auth skeleton

---
+++ avitrack-backend/Services/AuthService.cs
@@ -0,0 +1,15 @@
+using AviTrack.Api.Data;
+
+namespace AviTrack.Api.Services;
+
+public class AuthService
+{
+    private readonly AppDbContext _db;
+    private readonly IConfiguration _config;
+
+    public AuthService(AppDbContext db, IConfiguration config)
+    {
+        _db = db;
+        _config = config;
+    }
+}
