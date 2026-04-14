860fd1d05744c1d765bd4fc8a3df582f5e179f9a
Patryk Wojtkielewicz <p.wojtkielewicz1@gmail.com>
2026-04-14 15:14:14 +0200

    applying auth guard to protected routes

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -6,14 +6,15 @@ import { Dashboard } from './features/dashboard/dashboard';
 import { FlightDetail } from './features/flight-detail/flight-detail';
 import { AirportDetail } from './features/airport-detail/airport-detail';
 import { User } from './features/user/user';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'flights/:id', component: FlightDetail },
-  { path: 'airports/:id', component: AirportDetail },
-  { path: 'user', component: User },
+  { path: 'dashboard', component: Dashboard, canActivate: [AuthGuard] },
+  { path: 'flights/:id', component: FlightDetail, canActivate: [AuthGuard] },
+  { path: 'airports/:id', component: AirportDetail, canActivate: [AuthGuard] },
+  { path: 'user', component: User, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
